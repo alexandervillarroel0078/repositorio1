12e66974d7864598539964f6e297a18a7920acb3
alexandervillarroel007 <alexandervillarroels700@gmail.com>
2025-06-04 18:25:01 -0400

    1.2

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
 # run.py
 
 from app import create_app
+import os
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Necesario para Render
+    app.run(debug=True, host='0.0.0.0', port=port)
